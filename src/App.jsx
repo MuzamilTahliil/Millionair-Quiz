dc9ec505c6e675c0fa29afe864b339a6e0eb6355
MuzamilTahliil <muzamiltahliil1@gmail.com>
2024-07-16 16:22:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './pages/Login'
+
+
+function App() {
+  return(
+    <div>
+     {/* Routes */}
+     <Login />
+    </div>
+  )
+}
+export default App
