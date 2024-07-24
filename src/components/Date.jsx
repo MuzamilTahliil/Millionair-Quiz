fae8306a0b61ece85a9fdcb20908ecec9b408949
MuzamilTahliil <muzamiltahliil1@gmail.com>
2024-07-24 16:25:29 +0300

    .

---
+++ src/components/Date.jsx
@@ -0,0 +1,355 @@
+const data = [
+    {
+      id: 1,
+      question: "Sheeg magaca saxaabigii ugu horreeyey ee gala Kacbada isaga oo leh لبيك اللهم لبيك?  ",
+      answers: [
+        {
+          text: "A. Thamaama Bin Aathaal",
+          correct: true,
+        },
+        {
+          text:"B. Abuubakar Al-siddiq",
+          correct: false,
+        },
+        {
+          text: "C. Cali Bin Abii Dhaalib",
+          correct: false,
+        },
+        {
+          text: "D. Cukaasha Bin Mixsin",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "Sheeg magacyada saddex suuradood oo ku dhammaata sujuudu tilaawa ?",
+      answers: [
+        {
+          text: "A. Al- Acraaf , Anajmi , Al- Calaq",
+          correct: true,
+        },
+        {
+          text: "B. Al- Acraaf , Yasin , Al- Calaq",
+          correct: false,
+        },
+        {
+          text: "C. Al- Qasas,  Al- Baqra, Maa’ida",
+          correct: false,
+        },
+        {
+          text: "D. Al- Naxal, Al- Mudathir, Al- Mujaadala",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Sheeg  suuradda lagu magacaabo caruuska quraanka?",
+      answers: [
+        {
+          text: "A. Al-raxmaan ",
+          correct: true,
+        },
+        {
+          text: " B. Yasin ",
+          correct: false,
+        },
+        {
+          text: "C. Al- Faatiha",
+          correct: false,
+        },
+        {
+          text: "D. Al-iqlaas",
+          correct: false,
+        },
+      ],
+    },
+  
+    {
+      id: 4,
+      question: "Sheeg tirada ugu weyn ee lagu xusay quraanka kariimka?",
+      answers: [
+        {
+          text: "A. 1,000 (Kun)",
+          correct: false,
+        },
+        {
+          text: "B. 100 (Boqol)",
+          correct: false,
+        },
+        {
+          text: "C. 100,000 (Boqol Kun)",
+          correct: true,
+        },
+        {
+          text: "D. 1,000,000 (1Milyan)",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Waxaad sheegtaa gabadhii Alle u waxyooday?",
+      answers: [
+        {
+          text: "A. Fircoon Xaskiisa",
+          correct: false,
+        },
+        {
+          text: "B. Nebi muuse hooyadii",
+          correct: true,
+        },
+        {
+          text: "C. Boqoradii Balqiisa",
+          correct: false,
+        },
+        {
+          text: "D. Aisha Bintu Abii Bakar",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Waxaad sheegtaa suuradii sababta u ahayd inuu soo islaamo Cumar binu khadab?",
+      answers: [
+        {
+          text: "A. Al-Faatixa",
+          correct: false,
+        },
+        {
+          text: "B. Yasiin",
+          correct: false,
+        },
+        {
+          text: "C. Tabaarak",
+          correct: false,
+        },
+        {
+          text: "D. Daahaa",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: " Sheeg qofkii ugu horreeyey ee qora بسم الله الرحمن الرحيم ?",
+      answers: [
+        {
+          text: "A. nebi Suleymaan (C.S)",
+          correct: true,
+        },
+        {
+          text: "B. nabi Muhamed (C.S)",
+          correct: false,
+        },
+        {
+          text: "C. nabi Aadan (C.S)",
+          correct: false,
+        },
+        {
+          text: "D. nabi Ciise (C.S)",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Immisa salaadood ayey u dhigantaa salaadda lagu tukado Masjidka Xaramka ee Maka?",
+      answers: [
+        {
+          text: "A. 100,000 (Boqol Kun) oo salaadood",
+          correct: true,
+        },
+        {
+          text: "B. 100 (Boqol) oo salaadood",
+          correct: false,
+        },
+        {
+          text: "C. 1,000 (Kun) oo salaadood",
+          correct: false,
+        },
+        {
+          text: "D. hal(1) salad oo farad eh",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Sheeg magaca suurad ay aayad kasta oo ka mid uu ku jiro الله",
+      answers: [
+        {
+          text: "A. Al- Mulki",
+          correct: false,
+        },
+        {
+          text: "B.  Al- Mujaadala",
+          correct: true,
+        },
+        {
+          text: "C. Al- iqlaas",
+          correct: false,
+        },
+        {
+          text: "D. Al- Naxal",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Sheeg inta jeer ee quraanka kariimka lagu xusay مسلمين?",
+      answers: [
+        {
+          text: "A. 50 jeer",
+          correct: false,
+        },
+        {
+          text: "B.  23 jeer",
+          correct: false,
+        },
+        {
+          text: "C. 41 jeer",
+          correct: true,
+        },
+        {
+          text: "D. 51 jeer",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Sheeg suuradda lagu magacaabo suuradda Malaa’igta?",
+      answers: [
+        {
+          text: "A. Faadhir",
+          correct: true,
+        },
+        {
+          text: "B.  Towba",
+          correct: false,
+        },
+        {
+          text: "C. Muxamed",
+          correct: false,
+        },
+        {
+          text: "D. Al- Cimra",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Immisa jeer ayuu cimraysatay nebigu (C.S.W)?",
+      answers: [
+        {
+          text: "A. 12 jeer ayuu cimraysatay",
+          correct: false,
+        },
+        {
+          text: "B.  4 jeer ayuu cimraysatay",
+          correct: true,
+        },
+        {
+          text: "C. 1 jeer ayuu cimraysatay",
+          correct: false,
+        },
+        {
+          text: "D. 7 jeer ayuu cimraysatay",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Sheeg qofkii ugu horreeyey ee gabya?",
+      answers: [
+        {
+          text: "A. Xaaskii Fircoon",
+          correct: false,
+        },
+        {
+          text: "B.  Nabi Suleymaan (C.S)",
+          correct: false,
+        },
+        {
+          text: "C. zubeyr Binu Cawaam",
+          correct: false,
+        },
+        {
+          text: "D. Nabi Aadan ( C.S)",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: " Sheeg magacyada shanta suuradood ee ugu soo hor degtay quraanka kariimka?",
+      answers: [
+        {
+          text: "A. Al- Calaq, Al- Qalam, Al- Muzamil, Al- Mudathir, Al- Faatixa.",
+          correct: true,
+        },
+        {
+          text: "B. Al- Qalam, Al- Muzamil, Al- iqlaas, Al- Faatixa, Al- kaafiruun.",
+          correct: false,
+        },
+        {
+          text: "C. Al- iqlaas, Al- qalam, Al- Masad , Al- iqlaas, Al- Tabaarak.",
+          correct: false,
+        },
+        {
+          text: "D. Al- iqlaas, Al- iqlaas, Al- Tabaarak, Al- Qalam, Al- Qalam.",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "Waxaad sheegtaa magaca Malaga suurta afuufay oo kamid ah mallaigta?",
+      answers: [
+        {
+          text: "A. Israa’fiil",
+          correct: true,
+        },
+        {
+          text: "B.  Malakul mowt",
+          correct: false,
+        },
+        {
+          text: "C. Cazraa'iil",
+          correct: false,
+        },
+        {
+          text: "D Ridwaan",
+          correct: false,
+        },
+      ],
+    },
+    
+  ];
+  
+  const prizeMoney = [
+    { id: 1, amount: "$ 5000" },
+    { id: 2, amount: "$ 15000" },
+    { id: 3, amount: "$ 30000" },
+    { id: 4, amount: "$ 60000" },
+    { id: 5, amount: "$ 100000" },
+    { id: 6, amount: "$ 150000" },
+    { id: 7, amount: "$ 250000" },
+    { id: 8, amount: "$ 400000" },
+    { id: 9, amount: "$ 600000" },
+    { id: 10, amount: "$ 1000000" },
+    { id: 11, amount: "$ 5000000" },
+    { id: 12, amount: "$ 100000000" },
+    { id: 13, amount: "$ 300000000" },
+    { id: 14, amount: "$ 500000000" },
+    { id: 15, amount: "$ 1000000000" },
+  ].reverse();
+  
+  export { prizeMoney, data };
+  
