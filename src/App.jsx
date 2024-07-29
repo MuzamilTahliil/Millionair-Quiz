0392fa7dcbd84a79ca81992f48d1f93ac4dad8f1
MuzamilTahliil <muzamiltahliil1@gmail.com>
2024-07-29 09:31:44 +0300

    Add React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import Login from './pages/Login'
-
+import Login from './page/Login'
+import Home from './page/Home'
+import About from './page/About'
+import Contact from './page/Contact'
+import { Route, Routes } from 'react-router-dom'
+import QuizPage from './page/QuizPage'
+import NotFound from './components/NotFound'
 
 function App() {
   return(
     <div>
      {/* Routes */}
-     <Login />
+     
+     <Routes>
+      <Route path='/' element={< Home />} />
+      <Route path='/quiz' element={<QuizPage />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/About/' element={< About />} />
+      <Route path='*' element={<NotFound />} />
+     </Routes>
     </div>
   )
 }
