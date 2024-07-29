a3c94060111453d54853591d5f2fed5d2225e6c2
MuzamilTahliil <muzamiltahliil1@gmail.com>
2024-07-29 10:27:55 +0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,8 +12,8 @@ function Contact() {
         <Suspense fallback={<div>Loading Header...</div>}>
           <Header />
         </Suspense>
-        <div className="flex-grow bg-[#172554]">
-          <div className="font-[sans-serif] max-w-6xl mx-auto relative bg-white rounded-lg py-10 mt-32">
+        <div className="bg-gradient-to-b from-green-50 to-green-100 min-h-screen flex flex-col justify-between">
+          <div className="font-[sans-serif] max-w-6xl mx-auto relative  rounded-lg py-10 mt-32">
             <div className="grid lg:grid-cols-3 items-center">
               <div className="grid sm:grid-cols-2 gap-4 z-20 relative lg:left-16 max-lg:px-4">
                 <div className="flex flex-col items-center justify-center rounded-lg w-full h-44 p-4 text-center bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)]">
@@ -47,20 +47,20 @@ function Contact() {
                 <p className="text-sm text-gray-600 mt-2">+1-548-2588</p>
               </div>
             </div>
-            <div className="lg:col-span-2 bg-[#0a4275] rounded-lg sm:p-10 p-4 z-10 max-lg:-order-1 max-lg:mb-8">
+            <div className="lg:col-span-2 bg-[#6d28d9] rounded-lg sm:p-10 p-4 z-10 max-lg:-order-1 max-lg:mb-8">
               <h2 className="text-3xl text-white text-center font-bold mb-6">Contact us</h2>
               <form>
                 <div className="max-w-md mx-auto space-y-3">
                   <input type='text' placeholder='Name'
-                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-sm outline-none" />
+                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-black outline-none" />
                   <input type='email' placeholder='Email'
-                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-sm outline-none" />
+                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-black outline-none" />
                   <input type='email' placeholder='Phone No.'
-                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-sm outline-none" />
+                    className="w-full bg-gray-100 rounded-lg py-3 px-6 text-black outline-none" />
                   <textarea placeholder='Message' rows="6"
-                    className="w-full bg-gray-100 rounded-lg px-6 text-sm pt-3 outline-none"></textarea>
+                    className="w-full bg-gray-100 rounded-lg px-6 text-black pt-3 outline-none"></textarea>
                   <button type='button'
-                    className="text-gray-800 w-full relative bg-yellow-400 hover:bg-yellow-500 font-semibold rounded-lg text-sm px-6 py-3 !mt-6">
+                    className="text-gray-800 w-full relative bg-yellow-400 hover:bg-yellow-500 font-semibold rounded-lg text-black px-6 py-3 !mt-6">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill='currentColor' className="mr-2 inline" viewBox="0 0 548.244 548.244">
                       <path fillRule="evenodd" d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z" clipRule="evenodd" data-original="#000000" />
                     </svg>
